ee529d9cd1791af4c68e33f47ef23c7ed83b35bc
Abhishek P <75170385+abhishekkuttikkol@users.noreply.github.com>
2022-04-23 13:25:17 +0530

    fix: build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoHeaderComponent } from './todo-header/todo-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoHeaderComponent
   ],
   imports: [
     BrowserModule,
