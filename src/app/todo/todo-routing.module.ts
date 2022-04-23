70c30ee977136a669d767ef9ce2bc5434c965259
Abhishek P <abhishekkuttikkol007@gmail.com>
2022-04-23 07:50:44 +0000

    feat: todo app input and add button

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoHeaderComponent } from '../todo-header/todo-header.component';
+import { TodoBodyComponent } from './todo-body/todo-body.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: TodoHeaderComponent
+    component: TodoBodyComponent
   }
 ];
 
